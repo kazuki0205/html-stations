1293eeccdb86671ea24996ebb0e7973c1289ffb3
kazuki0205 <koidakazuki0505@gmail.com>
2024-05-30 18:37:46 +0900

    required

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
-}
 
-function test() {
-  return
-}
 
+  const test = (users) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const fullNames = users.map(user => ({
+          full_name: `${user.family_name} ${user.first_name}`
+        }));
+        resolve(fullNames);
+      }, 3000);
+    });
+  };
+
+  const result = await test(userList);
+  return result;
+
+}
